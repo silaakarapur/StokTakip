a5ccee5eedb02e25479b249229bc61aabe5f5aa0
silaakarapur <73967039+silaakarapur@users.noreply.github.com>
2022-01-06 18:01:05 +0300

    menu Activity

--- app/src/main/java/com/example/stoktakip/MenuActivity.java
+++ app/src/main/java/com/example/stoktakip/MenuActivity.java
@@ -2,26 +2,30 @@ package com.example.stoktakip;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
 public class MenuActivity extends AppCompatActivity {
+
     ImageButton imagebutton;
+    private Context getContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        imagebutton = findViewById(R.id.geç);
-        imagebutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MenuActivity.this,StockActivity.class);
-                startActivity(intent);
-            }
-        });
+
+
     }
 
 
+
+
+    public void git(View view) {
+        Intent git = new Intent(MenuActivity.this,StockActivity.class);
+        startActivity(git);
+    }
 }
